ba75c5da98e6130183dfd0d413f8a7d65a027819
Aulas <Aulas>
2017-10-18 00:04:13 -0200

    Aula 8 - Trabalhando com Vetores

---
+++ Aula8_TrabalhandoComVetores.py
@@ -0,0 +1,23 @@
+# Aula 8 - Trabalhando com arrays
+
+# Cachaça, coxinha, isqueiro, cigarro, ficha da sinuca (produtos)
+
+produtos = ["Cachaça", "Coxinha", "Isqueiro", "Cigarro", "Ficha da Sinuca"]
+
+# Endereçamento
+# produtos[0] = "Pingado"
+# produtos[5] = "Pingado"
+
+# Funções
+# append - Insiro um elemento no final do vetor
+# produtos.append("Pingado")
+# produtos.remove(produtos[0]) Remove o elemento do vetor
+# produtos.pop(0) - Retorna o valor do index e remove tal elemento do vetor
+
+# bebida = produtos[0]
+
+bebida = produtos.pop(0)
+
+print(bebida)
+
+print(produtos)
