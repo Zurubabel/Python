99f8c54ca0398cf3b2b004ccccde96cbdd0eace4
Zurubabel <zurubabel@zurubabel.com>
2018-09-05 00:05:44 -0300

    Primeiro commit do GNC com os nomes e apelidos já digitados

---
+++ GNC/GNC.py
@@ -0,0 +1,54 @@
+# 1 - Armazenar os nomes e apelidos de cachaceiros
+# 2 - Mês e o dia da pessoa
+
+mes = 11
+dia = 15
+
+# Mes - nome; Dia - apelido
+
+# Mês
+jan = "Betinho"
+fev = "Xexéu"
+mar = "Zé"
+abr = "Chicão"
+mai = "Elias"
+jun = "Ribamar"
+jul = "Arlindo"
+ago = "Sérgio"
+set = "Tião"
+out = "Bibica"
+nov = "Bira"
+dez = "Zeca"
+
+# Sobrenomes
+dia_1 = "Furico"
+dia_2 = "da Viola"
+dia_3 = "rosca solta"
+dia_4 = "Frasqueira"
+dia_5 = "Gambá"
+dia_6 = "Kokin"
+dia_7 = "Punheteiro"
+dia_8 = "Ku de Largata"
+dia_9 = "Doido"
+dia_10 = "Meu Mel"
+dia_11 = "Babaçu"
+dia_12 = "Carcaça trepada"
+dia_13 = "Alma Penada"
+dia_14 = "O crente das pinga"
+dia_15 = "das Mandingas"
+dia_16 = "cai torto"
+dia_17 = "chifrudo"
+dia_18 = "bola murcha"
+dia_19 = "do goró"
+dia_20 = "cana brava"
+dia_21 = "Macumbeiro"
+dia_22 = "171"
+dia_23 = "corno manso"
+dia_24 = "Bala Choca"
+dia_25 = "Freadão"
+dia_26 = "Vaca véia"
+dia_27 = "Nega Balaio"
+dia_28 = "Rola Cansada"
+dia_29 = "só cana"
+dia_30 = "Caganeira"
+dia_31 = "Ku de Mel"
