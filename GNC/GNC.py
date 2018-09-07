38fa4d6f63aadf76f421c17e2c685d1455b5b5fe
Zurubabel <zurubabel@zurubabel.com>
2018-09-06 23:59:59 -0300

    Aula 17 - Listas de nomes e apelidos

--- GNC/GNC.py
+++ GNC/GNC.py
@@ -1,6 +1,8 @@
 # 1 - Armazenar os nomes e apelidos de cachaceiros
 # 2 - Mês e o dia da pessoa
 
+# mês - 1 até 12
+# dia - 1 até 31
 mes = 11
 dia = 15
 
@@ -9,57 +11,21 @@ retorno = ""
 # Mes - nome; Dia - apelido
 
 # Mês
-jan = "Betinho"
-fev = "Xexéu"
-mar = "Zé"
-abr = "Chicão"
-mai = "Elias"
-jun = "Ribamar"
-jul = "Arlindo"
-ago = "Sérgio"
-set = "Tião"
-out = "Bibica"
-nov = "Bira"
-dez = "Zeca"
+nomes = ["Betinho", "Xexéu", "Zé", "Chicão", "Elias", "Ribamar",
+         "Arlindo", "Sérgio", "Tião", "Bibica", "Bira", "Zeca"]
 
 # Sobrenomes
-dia_1 = "Furico"
-dia_2 = "da Viola"
-dia_3 = "rosca solta"
-dia_4 = "Frasqueira"
-dia_5 = "Gambá"
-dia_6 = "Kokin"
-dia_7 = "Punheteiro"
-dia_8 = "Ku de Largata"
-dia_9 = "Doido"
-dia_10 = "Meu Mel"
-dia_11 = "Babaçu"
-dia_12 = "Carcaça trepada"
-dia_13 = "Alma Penada"
-dia_14 = "O crente das pinga"
-dia_15 = "das Mandingas"
-dia_16 = "cai torto"
-dia_17 = "chifrudo"
-dia_18 = "bola murcha"
-dia_19 = "do goró"
-dia_20 = "cana brava"
-dia_21 = "Macumbeiro"
-dia_22 = "171"
-dia_23 = "corno manso"
-dia_24 = "Bala Choca"
-dia_25 = "Freadão"
-dia_26 = "Vaca véia"
-dia_27 = "Nega Balaio"
-dia_28 = "Rola Cansada"
-dia_29 = "só cana"
-dia_30 = "Caganeira"
-dia_31 = "Ku de Mel"
+apelidos = ["Furico", "da Viola", "rosca solta", "Frasqueira", "Gambá", "Kokin", "Punheteiro",
+            "Ku de Largata", "Doido", "Meu Mel", "Babaçu", "Carcaça trepada", "Alma Penada",
+            "O crente das pinga", "das Mandingas", "cai torto", "chifrudo", "bola murcha", "do goró",
+            "cana brava", "Macumbeiro", "171", "corno manso", "Bala Choca", "Freadão",
+            "Vaca véia", "Nega Balaio", "Rola Cansada", "só cana", "Caganeira", "Ku de Mel"]
 
 # Decisão de qual nome retornar
-if mes == 1:
+"""if mes == 1:
     retorno = jan
 elif mes == 2:
     retorno = fev
 elif mes == 3:
-    retorno = mar
+    retorno = mar """
 
